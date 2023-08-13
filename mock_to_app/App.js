232441cd7b3a9ed1fe2a72f2f44183123e9ffeec
caisTec <137906658+Project-Vulcan@users.noreply.github.com>
2023-08-13 19:51:26 +0000

    late in development

---
+++ mock_to_app/App.js
@@ -0,0 +1,23 @@
+import { View, Text, TextInput} from "react-native";
+import { styles } from "./styles"
+
+export default function App() {
+  return (
+    <View
+      style={styles.homeScreen}
+    >
+
+      <Text style={styles.title}>MyApp</Text>
+
+      <View style={styles.middleBox}>
+
+        <TextInput placeholder="Phone Number, Username or Email" style={styles.inputBox}></TextInput>
+        <TextInput placeholder="Password" style={styles.inputBox}></TextInput>
+        <Text style={styles.loginButton}>Log In</Text>
+        <Text>Forgot Password?</Text>
+
+      </View>
+
+    </View>
+  );
+}
---
+++ mock_to_app/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    homeScreen: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5B8BDF",
+        width: "100%"
+      },
+
+    title: {color: "white", fontWeight: "bold", fontSize: 70},
+
+    middleBox: {width: "100%", alignItems: "center", flexDirection: "column", justifyContent: "space-evenly", height: "25%"},
+
+    inputBox: { backgroundColor: "white", width: "80%", fontSize: 15, padding: 5, paddingVertical: 10, paddingLeft: 15, borderWidth: 0.5, borderRadius: 20, overflow: "hidden"},
+
+    loginButton: { backgroundColor: "#2C64C6", color: "white", textAlign: "center", fontWeight: "bold", width: "80%", fontSize: 15, padding: 5, paddingVertical: 10, paddingLeft: 15, borderWidth: 0.5, borderRadius: 20, overflow: "hidden"}
+
+})
